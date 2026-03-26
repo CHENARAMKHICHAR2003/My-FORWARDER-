44db934fec24f300c198027e73d3f98a294f950e
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2026-03-26 22:49:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,9 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-
-# Don't Remove Credit Tg - @VJ_Botz
+    RAZORPAY_KEY_ID = environ.get("RAZORPAY_KEY_ID", "")
+    RAZORPAY_KEY_SECRET = environ.get("RAZORPAY_KEY_SECRET", "")
+#  Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
